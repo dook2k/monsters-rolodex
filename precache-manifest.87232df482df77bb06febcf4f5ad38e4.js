b0a562a37d153d77a3828a1f84637eab9f28d9dc
Walter Ashbrook <washbrook.wa@gmail.com>
2020-03-26 15:06:19 -0700

    Updates

---
+++ precache-manifest.87232df482df77bb06febcf4f5ad38e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17e07481242b9e6c60f72136d81cbd47",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8f0c853337b116c9b73e",
+    "url": "/monsters-rolodex/static/css/main.a65096c8.chunk.css"
+  },
+  {
+    "revision": "8a6eda421bc5765c55c2",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0c853337b116c9b73e",
+    "url": "/monsters-rolodex/static/js/main.29f9a538.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
